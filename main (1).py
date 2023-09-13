9897d649f2d9be0f1c20ae377a8cd2e0a15007d0
Sindhu1234567898 <144339123+Sindhu1234567898@users.noreply.github.com>
2023-09-13 15:21:05 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,71 +1,49 @@
-class BankAccount:
+class Player:
 
-    def __init__(self, account_number, account_holder_name, initial_balance=0):
+    def play(self):
 
-        self.__account_number = account_number
-
-        self.__account_holder_name = account_holder_name
-
-        self.__account_balance = initial_balance
-
-
-
-    def deposit(self, amount):
-
-        if amount > 0:
-
-            self.__account_balance += amount
-
-            print(f"Deposited {amount} units. New balance: {self.__account_balance}")
-
-        else:
-
-            print("Deposit amount must be greater than zero.")
-
-
-
-    def withdraw(self, amount):
-
-        if 0 < amount <= self.__account_balance:
-
-            self.__account_balance -= amount
-
-            print(f"Withdrew {amount} units. New balance: {self.__account_balance}")
-
-        else:
-
-            print("Withdrawal amount must be greater than zero and less than or equal to the account balance.")
-
-
-
-    def display_balance(self):
-
-        print(f"Account Holder: {self.__account_holder_name}")
-
-        print(f"Account Number: {self.__account_number}")
-
-        print(f"Account Balance: {self.__account_balance}")
+        print("The player is playing cricket.")
 
 
 
 
 
-# Testing the BankAccount class
+class Batsman(Player):
+
+    def play(self):
+
+        print("The batsman is batting.")
+
+
+
+
+
+class Bowler(Player):
+
+    def play(self):
+
+        print("The bowler is bowling.")
+
+
+
+
+
+# Testing the Player, Batsman, and Bowler classes
 
 if __name__ == "__main__":
 
-    account = BankAccount("1234567890", "John Doe", 1000)
+    player = Player()
+
+    player.play()  # Output: The player is playing cricket.
 
 
 
-    account.display_balance()  # Should display the initial balance
+    batsman = Batsman()
+
+    batsman.play()  # Output: The batsman is batting.
 
 
 
-    account.deposit(500)  # Depositing 500 units
+    bowler = Bowler()
 
-    account.withdraw(200)  # Withdrawing 200 units
-
-
-
-    account.display_balance()  # Should display the updated balance after transaction
+    bowler.play()  # Output: The bowler is bowling
